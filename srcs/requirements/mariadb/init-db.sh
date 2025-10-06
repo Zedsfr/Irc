55c8267120ad37bf2734490d2c91580371431006
Zedsfr <167260856+Zedsfr@users.noreply.github.com>
2025-10-06 18:00:40 +0200

    Add files via upload

--- srcs/requirements/mariadb/init-db.sh
+++ srcs/requirements/mariadb/init-db.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 set -e
 
-mysqld_safe --skip-networking --skip-grant-tables &
+DATADIR="/var/lib/mysql"
 
-until mysqladmin --protocol=socket ping --silent; do
+# Forcer la propriété du dossier data
+chown -R mysql:mysql "$DATADIR"
+
+if [ ! -d "$DATADIR/mysql" ]; then
+  echo "Initialisation de la base de données..."
+  mariadb-install-db --user=mysql --datadir="$DATADIR"
+
+  mysqld_safe --skip-networking --skip-grant-tables &
+
+  until mysqladmin ping --silent; do
     sleep 1
-done
+  done
 
-mysql --protocol=socket -u root <<SQL
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-SQL
+  mysql <<-EOSQL
+    FLUSH PRIVILEGES;
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+    FLUSH PRIVILEGES;
+EOSQL
 
-mysqladmin --protocol=socket -uroot -p"${MYSQL_ROOT_PASSWORD}" shutdown || true
+  mysqladmin -uroot -p"${MYSQL_ROOT_PASSWORD}" shutdown
+  echo "Initialisation terminée"
+fi
 
 exec mysqld_safe
